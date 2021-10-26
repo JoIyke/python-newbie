1148607e86f28a0ce13fa65e977dff2b354a3e47
Joshua Okonkwo <joshuaokonkwo@gmail.com>
2021-10-26 09:11:13 +0100

    final submission for now

---
+++ fastll.py
@@ -0,0 +1,90 @@
+import requests
+import os
+PORT = int(os.environ.get('PORT', 5000))
+
+import datetime 
+from time import sleep
+
+url = "https://api.telegram.org/bot2026543791:AAGRbnN4RhzF4VjmZBfsFBMFd9Tq-FFCngA/"
+
+
+def get_updates_json(request):
+    response = requests.get(request + 'getUpdates')
+    return response.json()
+
+
+def last_update(data):
+    results = data['result']
+    total_updates = len(results) - 1
+    return results[total_updates]
+
+
+def get_chat_id(update):
+    chat_id = update['message']['chat']['id']
+    return chat_id
+
+def send_mess(chat, text):
+    params = {'chat_id': chat, 'text': text}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
+
+chat_id = get_chat_id(last_update(get_updates_json(url)))
+
+msg = "What do you want to send?: "
+send_mess(chat_id, msg)
+
+def main():
+    update_id = last_update(get_updates_json(url))['update_id']
+    while True:
+        if update_id == last_update(get_updates_json(url))['update_id']:
+           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
+           update_id += 1
+    sleep(1)       
+
+if __name__ == '__main__':
+    main()
+
+greet_bot = BotHandler(token)
+greetings = ('hello', 'hi', 'greetings', 'sup')
+now = datetime.datetime.now()
+
+
+def main():
+    new_offset = None
+    today = now.day
+    hour = now.hour
+
+    while True:
+        greet_bot.get_updates(new_offset)
+
+        last_update = greet_bot.get_last_update()
+
+        last_update_id = last_update['update_id']
+        last_chat_text = last_update['message']['text']
+        last_chat_id = last_update['message']['chat']['id']
+        last_chat_name = last_update['message']['chat']['first_name']
+
+        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+            greet_bot.send_message(last_chat_id, 'Good Morning  {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+            greet_bot.send_message(last_chat_id, 'Good Afternoon {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+            greet_bot.send_message(last_chat_id, 'Good Evening  {}'.format(last_chat_name))
+            today += 1
+
+        new_offset = last_update_id + 1
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
+
+
+
+
+
